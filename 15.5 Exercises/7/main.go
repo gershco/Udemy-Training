54d9c9997edcf904b9cd9594fd5a52aeb3c221f7
gershco <31931276+gershco@users.noreply.github.com>
2017-10-19 18:17:33 +0100

    15.5 Questions 7 and 8

---
+++ 15.5 Exercises/7/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var s string
+
+	s = `There was once a man from Dunblane,
+Who regularly travelled by plane.
+One day while embarking,
+He was joking and larking,
+And at the airport he had to remain!`
+
+	fmt.Println(s)
+
+}
---
+++ 15.5 Exercises/8/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	a := "Always look on the bright side of life!"
+
+	slice := []byte(a)
+
+	fmt.Println(slice)
+
+}
